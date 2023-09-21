9d05b596f41aa904ed19f2470661894737d2be98
Jesús Andrés Monge <jesus_jaml@outlook.com>
2023-09-21 16:28:01 -0500

    [UPD]  header and hero improvements
    commit 251feab85a31f6fac535e756c56c9b6e64b6cb42
    Author: Jesús Andrés Monge <jesus_jaml@outlook.com>
    Date:   Thu Sep 21 16:23:28 2023 -0500
    
        [UPD] Hero and Logo componentes
        Wave animation
        Logos for supported themes
    
    commit b8dcd6af238bf153e7b616bf52cf61e33caee03c
    Author: Jesús Andrés Monge <jesus_jaml@outlook.com>
    Date:   Thu Sep 21 14:47:03 2023 -0500
    
        [FIX] text depending on screen size
        MobileNav use variant modern
    
    commit eaa82135379ceb699dca7269fcef1129ce1aab45
    Author: Jesús Andrés Monge <jesus_jaml@outlook.com>
    Date:   Thu Sep 21 00:27:55 2023 -0500
    
        [UPD] Hero and Navbar
        Improve responsive
        Add button variant
    
    commit f7d164e9dc28d99d67639b9c74ef0603190ce0e5
    Author: Jesús Andrés Monge <jesus_jaml@outlook.com>
    Date:   Wed Sep 20 18:19:22 2023 -0500
    
        [FEAT] Add responsive Hero section
    
    commit 0760f6e2eef667c010c329092dcaef5eb3a2f014
    Author: Jesús Andrés Monge <jesus_jaml@outlook.com>
    Date:   Tue Sep 19 17:20:02 2023 -0500
    
        [UPD] Upgrade next from 13.4.19 to 13.5.1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,35 +2,42 @@ import Image from 'next/image'
 import Socials from './Socials'
 import { Button } from './ui/button'
 import { cn } from '../lib/utils'
-import { SITE } from '@/config/site'
 import { SOCIALS } from '@/config/icons'
+import { Circle } from 'lucide-react'
 
 function Hero () {
   const socialCount = SOCIALS.filter((social) => social.active).length
 
   return (
-    <div className={cn(`h-[calc(100vh-${SITE.headerHeight}px)] pt-20 flex flex-col justify-between md:flex-row md:items-center md:justify-center md:space-x-16 md:p-0`)}>
+    <div className={cn('py-6 md:py-32 flex flex-col justify-between md:flex-row md:items-center md:justify-center md:space-x-16')}>
       {/* Introduction + CTA  */}
-      <div className='max-w-sm'>
-        <div className='flex flex-col '>
-          <h1 className='text-2xl md:text-4xl m-0 mb-4 md:mb-0'>Hey! <span className='animate-wave'>👋</span> <br />
-            I'm <span className='font-bold text-primary'>Jesus Monge</span>
-          </h1>
-          <p className='text-popover-foreground font-medium'>Fullstack Developer <br /> <span className='text-muted-foreground font-normal'>Passionate for crafting solutions that drive tangible results</span>
-          </p>
+      <div className='max-w-sm space-y-4 md:space-y-6'>
+        <div className='flex flex-col'>
+          <h2 className='text-2xl md:text-4xl'>
+            <span>Hey! <span className='animate-wave-animation inline-block origin-top-left-3/4-3/4'>👋</span></span>
+            <br />
+            I’m <span className='text-primary'>Jesús Monge</span>
+          </h2>
+          <div className='mb-1 md:mb-2'>
+            <div className='flex items-center space-x-2'>
+              <Circle className='text-primary fill-primary' size={8} />
+              <span className='text-popover-foreground md:text-xl m-0'>Fullstack Developer </span>
+            </div>
+          </div>
+          <span className='text-muted-foreground font-normal md:text-lg'>Passionate for crafting solutions that drive tangible results</span>
         </div>
-        <div className='space-x-4 mb-6'>
+        <div className='space-x-4'>
           <Button>Resume</Button>
           <Button variant='outline'>View Projects</Button>
         </div>
         {socialCount > 0
           ? (
-            <Socials className='mb-12' />
+            <Socials className='mb-12 md:mb-0' />
             )
           : null}
       </div>
       {/* Hero Image  */}
-      <div className='pb-12 md:p-0 '>
+      <div className='max-w-md mx-auto'>
         <Image
           src='/images/hero1.svg'
           alt='Hero Image'
